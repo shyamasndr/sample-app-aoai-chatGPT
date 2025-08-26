e17582848daa059fea003259fde9e1a4a28f16c5
Shyamasundara Kudkuli <shyamasundara.kudkuli@volvo.com>
2025-08-26 12:56:19 +0530

    modified styles

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -880,7 +880,7 @@ const Chat = () => {
                       root: {
                         color: '#FFFFFF',
                         background:
-                          'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
+                          '#3104f8ff'
                       },
                       rootDisabled: {
                         background: '#F0F0F0'
@@ -905,7 +905,7 @@ const Chat = () => {
                     root: {
                       color: '#FFFFFF',
                       background:
-                        'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
+                        '#3104f8ff'
                     },
                     rootDisabled: {
                       background: '#F0F0F0'
